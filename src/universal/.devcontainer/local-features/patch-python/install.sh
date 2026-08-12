d8dec89cb8f979d7b978bb21c3dc9dc8fd365861
q32757468 <32757468@qq.com>
2026-08-12 12:07:17 +0800

    chore(universal): switch package mirrors to Alibaba Cloud

--- src/universal/.devcontainer/local-features/patch-python/install.sh
+++ src/universal/.devcontainer/local-features/patch-python/install.sh
@@ -7,12 +7,12 @@ echo "(*) Patching Python pip registry mirrors..."
 
 REMOTE_USER_HOME="$(get_remote_user_home)"
 
-# pip mirror (Tsinghua)
+# pip mirror (Alibaba Cloud)
 mkdir -p "${REMOTE_USER_HOME}/.config/pip"
 cat > "${REMOTE_USER_HOME}/.config/pip/pip.conf" << 'EOF'
 [global]
-index-url = https://pypi.tuna.tsinghua.edu.cn/simple
-trusted-host = pypi.tuna.tsinghua.edu.cn
+index-url = https://mirrors.aliyun.com/pypi/simple/
+trusted-host = mirrors.aliyun.com
 EOF
 
 chown -R "${_REMOTE_USER}:${_REMOTE_USER}" "${REMOTE_USER_HOME}/.config/pip"
