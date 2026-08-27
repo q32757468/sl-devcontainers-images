6a19c43ee0c653d648642cb537c054f559e2823b
q32757468 <32757468@qq.com>
2026-08-28 00:27:16 +0800

    fix(patch-node): configure npm registry for build user

--- src/universal/.devcontainer/features/src/patch-node/install.sh
+++ src/universal/.devcontainer/features/src/patch-node/install.sh
@@ -5,6 +5,10 @@ source /usr/local/share/devcontainer-features/utils/utils.sh
 
 echo "(*) Patching npm registry mirrors..."
 
+# npm mirror for the user running the Feature installation (usually root).
+# This is also available to npm commands run by later image-build steps.
+printf "%s\n" "registry=https://registry.npmmirror.com/" > "${HOME}/.npmrc"
+
 # npm mirror
 run_as_remote_user \
     sh -c 'printf "%s\n" "registry=https://registry.npmmirror.com/" > "${HOME}/.npmrc"'
